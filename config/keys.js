ed21ae3dc9188710414aa04326534f5137d34408
Anthony Zhang <anthonyzhang1997@gmail.com>
2021-09-17 04:53:46 -0400

    hiding API KEY

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports ={
-    mongoURI:'mongodb+srv://Anthony:zhangxiangyu1997@laoma-queueing.binqb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    mongoURI: process.env.API_KEY
 }
