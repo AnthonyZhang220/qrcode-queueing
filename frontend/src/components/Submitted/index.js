0c7db6c8aa306fed4a05b65bd2c38ee47cea1d62
Anthony Zhang <anthonyzhang1997@gmail.com>
2021-09-19 13:49:09 -0400

    deploy

--- frontend/src/components/Submitted/index.js
+++ frontend/src/components/Submitted/index.js
@@ -4,12 +4,16 @@ import axios from "axios";
 
 class Submitted extends Component {
 	state = {
-		queue: '',
+		queue: "",
+		name: "",
+		numberofpeople: "",
 	};
 
 	componentDidMount() {
 		axios.get("/api/items").then((res) => {
 			this.setState({ queue: `${res.data.length}` });
+			this.setState({ name: `${res.data[res.data.length - 1].name}` });
+			this.setState({ numberofpeople: `${res.data[res.data.length - 1].numberofpeople}` });
 		});
 	}
 
@@ -18,9 +22,11 @@ class Submitted extends Component {
 			<div className={styles.container}>
 				<div className={styles.result_background}>
 					<div className={styles.result}>
-						<h4>Your Queue Number is: </h4>
+						<h3>Your Queue Number is: </h3>
 						<h1>{this.state.queue}</h1>
-						<p>Please do not refresh this page.</p>
+						<h3>Guest: {this.state.name}</h3>
+						<h3>Table for: {this.state.numberofpeople} </h3>
+						<p>Please DO NOT refresh this page.</p>
 					</div>
 				</div>
 			</div>
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const path = require('path');
 
 const items = require("./routes/api/items");
 
@@ -21,7 +22,15 @@ mongoose
 
 
 //Use Routes
-app.use('/api/items', items)
+app.use('/api/items', items);
+
+if(process.env.NODE_ENV === 'production'){
+	app.use(express.static('frontend/build'));
+
+	app.get('*', (req, res)=> {
+		res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+	});
+}
 
 const port = process.env.PORT || 5000;
 
